2c363061e2cea798a2d8ccb4e15f1a0fc171bc09
marcio henrique <mhmuzy857@gmail.com>
2020-11-11 15:21:31 -0300

    Implementação na Classe Usuário [Treinamento]

--- src/biblioteca/Usuario.java
+++ src/biblioteca/Usuario.java
@@ -3,5 +3,13 @@ package biblioteca;
 public class Usuario {
 
 	private int id;
-	private String usuario;
+	private String nome;
+	
+	public void cadastrar() {
+		
+	}
+	
+	public void excluir() {
+		
+	}
 }
