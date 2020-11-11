dbc5f561cd5eb705170b578e065b073d29b00b6b
marcio henrique <mhmuzy857@gmail.com>
2020-11-11 14:23:53 -0300

    Merge Branch 'Master' [Treinamento]

--- src/biblioteca/Livro.java
+++ src/biblioteca/Livro.java
@@ -3,4 +3,14 @@ package biblioteca;
 public class Livro {
 	
 	private String titulo;
+	private int numPaginas;
+	private String autor;
+	
+	public void emprestar() {
+		
+	}
+	
+	public void devolver() {
+		
+	}
 }
